1c647ffa6f9eba266524cfaed9e08e62fad116cf
Edgar Alemasov <edgar.alemasov@gmail.com>
2023-01-18 21:42:45 +0500

    destructuring function

---
+++ src/js/destructuring.js
@@ -0,0 +1,17 @@
+export default function destructuring (character) {
+    const targetArray = [];
+    for (let object of character["special"]) {
+      if (object["description"] === undefined) {
+        const information = {
+          id: object["id"],
+          name: object["name"],
+          icon: object["icon"],
+          description: "Описание недоступно"
+        };
+        targetArray.push(information);
+      } else {
+        targetArray.push(object);
+      }
+    };
+    return targetArray
+}
